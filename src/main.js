595009dedf9a20774dc341b61f31e04fcaf5156f
João <jmmsp2003@hotmail.com>
2022-11-03 18:34:26 -0300

    Your Message

---
+++ src/main.js
@@ -0,0 +1,52 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+              <img src="assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+              <strong>${hour}</strong>
+              <img src="assets//icon-${player2}.svg" alt="Bandeira do ${player2}" />
+            </li>`
+}
+
+function createCard(date, day, games) {
+  return `
+  <div class="card">
+          <h2>${date}<span>${day}</span></h2>
+          <ul>
+            ${games}
+          </ul>
+        </div>`
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "Domingo", createGame("qatar", "08:00", "ecuador")) +
+  createCard(
+    "21/11",
+    "Segunda",
+    createGame("england", "10:00", "iran") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("united estates", "16:00", "wales")
+  ) +
+  createCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "07:00", "saudi arabia") +
+      createGame("denmark", "10:00", "tunisia") +
+      createGame("mexico", "13:00", "poland") +
+      createGame("france", "16:00", "australia")
+  ) +
+  createCard(
+    "23/11",
+    "Quarta",
+    createGame("morocco", "07:00", "croatia") +
+      createGame("germany", "10:00", "japan") +
+      createGame("spain", "13:00", "costa rica") +
+      createGame("belgium", "16:00", "canada")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "south korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  )
